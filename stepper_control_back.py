dff2645d986146f051bccc1f8d6e4218dccd602e
Jeremy Schreiber <86371980+jschreib1@users.noreply.github.com>
2021-11-01 19:13:47 +0000

    Moved a thing

--- stepper_control_back.py
+++ stepper_control_back.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import time
 from stepper import Stepper
 from PCF import PCF8591
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 pins = [18,21,22,23] # controller inputs: in1, in2, in3, in4
 for pin in pins:
@@ -11,11 +12,13 @@ GPIO.setup(12, GPIO.IN)
 # two adjacent phases must be actuated together before stepping to
 # a new phase so that the rotor is pulled in the right direction:
 
+state = 0 # current position in stator sequence
+current_angle = 0
+
 sequence = [ [1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],
 [0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1] ]
 
-state = 0 # current position in stator sequence
-current_angle = 0
+
 
 try:
   myADC = PCF8591(0x48)
