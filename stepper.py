533a3f1eb99f34cbaf6851ec306134223da4649f
Jeremy Schreiber <86371980+jschreib1@users.noreply.github.com>
2021-11-03 14:55:27 +0000

    h

--- stepper.py
+++ stepper.py
@@ -7,17 +7,21 @@ GPIO.setwarnings(False)
 
 
 class Stepper:
+  pins = [18,21,22,23] # controller inputs: in1, in2, in3, in4
+  sequence = [ [1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],
+    [0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1] ]
+  state = 0 # current position in stator sequence
+  current_angle = 0
+  led_pin = 16
+
   def __init__(self, address):
     self.PCF8591 = PCF8591(address)
     #PCF8591(0x40)
-    self.state = 0 # current position in stator sequence
-    self.current_angle = 0
+
     GPIO.setmode(GPIO.BCM)
-    self.pins = [18,21,22,23] # controller inputs: in1, in2, in3, in4
     for pin in self.pins:
       GPIO.setup(pin, GPIO.OUT, initial=0)
     GPIO.setup(12, GPIO.IN)
-    self.led_pin = 16
     GPIO.setup(self.led_pin, GPIO.OUT)
     # Define the pin sequence for counter-clockwise motion, noting that
     # two adjacent phases must be actuated together before stepping to
@@ -25,8 +29,7 @@ class Stepper:
 
 
 
-    self.sequence = [ [1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],
-    [0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1] ]
+    
   
   def getval(self):
     print(self.PCF8591.read(0))
@@ -35,11 +38,11 @@ class Stepper:
 
   def halfstep(self, dir):
     #dir = +/- 1 (ccw /cw)
-    self.state += dir
-    if self.state > 7: self.state = 0
-    elif self.state < 0: self.state = 7
+    Stepper.state += dir
+    if Stepper.state > 7: Stepper.state = 0
+    elif Stepper.state < 0: Stepper.state = 7
     for pin in range(4):
-      GPIO.output(self.pins[pin], self.sequence[self.state][pin])
+      GPIO.output(Stepper.pins[pin], Stepper.sequence[Stepper.state][pin])
 
   def moveSteps(self, steps, dir):
     # move the actuation sequence a given number of half steps
@@ -54,13 +57,12 @@ class Stepper:
       self.moveSteps(500,1)
       light = self.PCF8591.read(0)
     print(self.PCF8591.read(0))
-    GPIO.output(self.led_pin, 0)
-    self.current_angle = 0
-    print(self.current_angle)
-    return self.current_angle
+    GPIO.output(Stepper.led_pin, 0)
+    Stepper.current_angle = 0
+    print(Stepper.current_angle)
 
   def goAngle(self, angle):
-    turn_angle = angle - self.current_angle
+    turn_angle = angle - Stepper.current_angle
     if abs(turn_angle) < 180:
       self.moveSteps(angle,1)
     else:
