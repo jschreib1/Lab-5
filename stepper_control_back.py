7632e4079db1dcb7d8aa736f3770b666a148cb44
Jeremy Schreiber <86371980+jschreib1@users.noreply.github.com>
2021-11-03 14:59:26 +0000

    k

--- stepper_control_back.py
+++ stepper_control_back.py
@@ -9,9 +9,6 @@ try:
   myADC = PCF8591(0x48)
   myStepper = Stepper(0x48)
   light_level = myStepper.getval()
-  while True:
-    myStepper.halfstep(1)
-    print('hello')
   myStepper.zero()
   #myStepper.goAngle(90)
 
