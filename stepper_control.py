e3f7404e6a9284cfa209244d35839376bb2909dc
Jeremy Schreiber <86371980+jschreib1@users.noreply.github.com>
2021-11-03 16:39:15 +0000

    w

--- stepper_control.py
+++ stepper_control.py
@@ -3,11 +3,8 @@
 import cgi
 import json
 import cgitb
-from urllib.request import urlopen
-from urllib.parse import urlencode
 import time
 cgitb.enable()
-api = "PWRX31PF18M31AG0"
 
 print("Content-type: text/html\n\n")
 data= cgi.FieldStorage()
@@ -17,23 +14,7 @@ data = {"Angle":s1,"Button":s2}
 with open('angle.txt', 'w') as f:
   json.dump(data,f)
 
-#params stuff
-while True:
-  params = {
-    "api_key":api,
-    1: Angle,
-    2: Button,}
-  params = urlencode(params)   # put dict data into a GET string
-
-  # add "?" to URL and append with parameters in GET string:
-  url = "https://api.thingspeak.com/update?" + params
-  try:
-    response = urlopen(url)      # open the URL to send the request
-    print(response.status, response.reason)  # display the response
-    print(response.read()) # display response page data
-    time.sleep(16)    # 15 sec minimum
-  except Exception as e:
-    print(e)
+  
 
 print('<html>')
 print('<form action="/cgi-bin/stepper_control.py" method="POST">')
--- stepper_control_back.py
+++ stepper_control_back.py
@@ -3,6 +3,10 @@ from PCF import PCF8591
 from stepper import Stepper
 import time
 import json
+from urllib.request import urlopen
+from urllib.parse import urlencode
+
+api = "PWRX31PF18M31AG0"
 
 
 
@@ -22,4 +26,20 @@ try:
 except Exception as e:
   print(e)
   pass
+
+params = {
+    "api_key":api,
+    1: Angle,
+    2: Button,}
+  params = urlencode(params)   # put dict data into a GET string
+
+  # add "?" to URL and append with parameters in GET string:
+  url = "https://api.thingspeak.com/update?" + params
+  try:
+    response = urlopen(url)      # open the URL to send the request
+    print(response.status, response.reason)  # display the response
+    print(response.read()) # display response page data
+    time.sleep(16)    # 15 sec minimum
+  except Exception as e:
+    print(e)
 GPIO.cleanup()
